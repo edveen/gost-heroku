ebc89b8b3599cb64498eb34a32787e5f39cde0be
edveen <19629858+edveen@users.noreply.github.com>
2020-05-23 22:42:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /gost
-wget -O gost.gz https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
+wget -O gost.tar.gz https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz
 gunzip -c gost.gz > gost
 chmod +x gost
 ./gost -L $MODE://:$PORT
